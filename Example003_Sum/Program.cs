001565486da2afea694f2eceba7dfa6a30b24d52
yauhenia <yauhenia13@gmail.com>
2023-02-14 01:22:54 +0300

    Add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,11 @@
-﻿int num1 = 31;
-int num2 = 8;
+﻿// int num1 = 31;
+// int num2 = 8;
+// int current = num1 + num2;
+// Console.WriteLine(current);
+
+int num1 = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(num1);
+int num2 = new Random().Next(1, 10);
+Console.WriteLine(num2);
 int current = num1 + num2;
 Console.WriteLine(current);
