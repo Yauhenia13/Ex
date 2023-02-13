881621e3257d3cdee865602b669131606a23b712
yauhenia <yauhenia13@gmail.com>
2023-02-14 00:25:13 +0300

    Example002 Hello user

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,9 @@
+﻿// Console.WriteLine("Input your name ");
+// string username = Console.ReadLine();
+// Console.WriteLine("Hello, ");
+// Console.WriteLine(username);
+
+Console.Write("Input your name ");
+string username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username);
