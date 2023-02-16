a9fcb69da773fcef3196ef4e803a4c34fee34619
yauhenia <yauhenia13@gmail.com>
2023-02-16 14:41:13 +0300

    Add folder Ex010

---
+++ Ex010/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {11, 202, 35, 7, 408, 45, 65, 7, 18};
+
+int n = array.Length; //-> длина/колличество элементов массива
+int find = 7;
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // если в массиве два одинаковых элемента
+    }
+    index++;
+}
